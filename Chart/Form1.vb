32c94e2610efba20785704ce864432c18c8a23f5
Marco Chávez <totto_cha@hotmail.com>
2016-04-18 16:59:22 -0600

    Uso de chart en visual basic .net

---
+++ Chart/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Grafica
+
+    Private Sub Grafica_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Chart1.Dock = DockStyle.Fill
+        Chart1.Series.Add("Nombre")
+        Chart1.Series("Nombre").ChartType = DataVisualization.Charting.SeriesChartType.Line
+        Chart1.Series("Nombre").Points.AddXY(5, 10)
+        Chart1.Series("Nombre").Points.AddXY(10, 5)
+        Chart1.Series("Nombre").Points.AddXY(15, 20)
+        Chart1.Series("Nombre").Points.AddXY(20, 5)
+        Chart1.Series("Nombre").ChartArea = "ChartArea1"
+
+    End Sub
+End Class
